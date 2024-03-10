8c8e358f3f98ae170704bf57700743736ef97138
Mehrshin <52357616+Mehrshin@users.noreply.github.com>
2024-03-10 17:21:44 -0400

    remove import react

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,5 +1,5 @@
 // src/context/Auth.tsx
-import React, { createContext, useState, ReactNode } from 'react';
+import { createContext, useState, ReactNode } from 'react';
 
 export interface AuthType {
     user:{
