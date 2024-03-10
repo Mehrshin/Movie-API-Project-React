132eef66bad92f437d1c621ceced2c63e5fb787c
Mehrshin <52357616+Mehrshin@users.noreply.github.com>
2024-03-10 17:22:25 -0400

    remove import react

--- src/components/Favorite.tsx
+++ src/components/Favorite.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar as solidStar } from '@fortawesome/free-solid-svg-icons';
 import { faStar as regularStar } from '@fortawesome/free-regular-svg-icons';
